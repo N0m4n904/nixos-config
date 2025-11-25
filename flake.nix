b34e3b7e622a6c8ee7b80e7d17dce285a577231a
Noah Anleitner <noah.anleitner@nonetwor.cc>
2025-11-25 18:45:12 +0100

    Update flake.lock and linux-nitrous

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
     linux-nitrous = {
-      url = "file+https://gitlab.com/xdevs23/linux-nitrous/-/raw/v6.17.8-2-nixos/default.nix";
+      url = "file+https://gitlab.com/xdevs23/linux-nitrous/-/raw/v6.17.9-1-nixos/default.nix";
       flake = false;
     };
     home-manager = {
