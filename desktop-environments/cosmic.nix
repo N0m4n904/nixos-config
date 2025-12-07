ad4b11e7e4e5904fd272071198d582dc3b22839d
Noah Anleitner <noah.anleitner@nonetwor.cc>
2025-12-07 23:03:36 +0100

    add extensions to cosmic

--- desktop-environments/cosmic.nix
+++ desktop-environments/cosmic.nix
@@ -1,6 +1,12 @@
-{ foundrixModules, ... }:
+{ foundrixModules, mkPerUserHomeManagerModule, pkgs, ... }:
 {
   imports = [
     foundrixModules.components.desktop-environments.cosmic
+    (mkPerUserHomeManagerModule __curPos {
+      home.packages = with pkgs; [
+          cosmic-ext-applet-caffeine
+          cosmic-ext-applet-minimon
+      ];
+    })
   ];
 }
