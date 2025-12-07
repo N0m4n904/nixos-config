63e2b138172238ea217ff5784eccf6e6a6e1c5e4
Noah Anleitner <noah.anleitner@nonetwor.cc>
2025-12-07 17:10:43 +0100

    add ccache drive

--- devices/pteranodon/filesystems.nix
+++ devices/pteranodon/filesystems.nix
@@ -33,6 +33,16 @@
         "noatime"
       ];
     };
+
+    "/home/noah/ccache" = {
+      device = "/dev/disk/by-uuid/a511957e-372d-426a-9f9f-efa9e2140ca5";
+      fsType = "btrfs";
+      options = [
+        "rw"
+        "noatime"
+        "compress=zstd:4"
+      ];
+    };
   };
   boot.supportedFilesystems = [ "btrfs" ];
 }
