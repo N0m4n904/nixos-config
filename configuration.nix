55ffbc41173273d62ab2985f420f1eb25c4d01bd
Noah Anleitner <noah.anleitner@nonetwor.cc>
2025-12-30 22:36:40 +0100

    Proton-CachyOS: use v4

--- configuration.nix
+++ configuration.nix
@@ -97,11 +97,11 @@ in
       (proton-ge-bin.overrideAttrs (
         prev: final: {
           src = fetchzip {
-            url = "https://github.com/CachyOS/proton-cachyos/releases/download/cachyos-10.0-20251126-slr/proton-cachyos-10.0-20251126-slr-x86_64_v3.tar.xz";
-            hash = "sha256-aKHEBR1q43Uk4JlXE3b8TFllVj1UJ7t6vZmXQj4M800=";
+            url = "https://github.com/CachyOS/proton-cachyos/releases/download/cachyos-10.0-20251126-slr/proton-cachyos-10.0-20251126-slr-x86_64_v4.tar.xz";
+            hash = "sha256-wSPVOnGyEej4RMIBUnQ6aj1n3KiZ8ftNq/hpa4+1Q/8=";
           };
           pname = "proton-cachyos";
-          version = "proton-cachyos-10.0-20251126-slr-x86_64_v3";
+          version = "proton-cachyos-10.0-20251126-slr-x86_64_v4";
         }
       )).override
       { steamDisplayName = "Proton-CachyOS"; }
