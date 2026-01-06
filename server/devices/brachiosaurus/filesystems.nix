78a6a2fc289bd569a7953ef57238a474cdbaba92
Noah Anleitner <noah.anleitner@nonetwor.cc>
2026-01-06 14:15:38 +0100

    brachiosaurus: add drives

--- server/devices/brachiosaurus/filesystems.nix
+++ server/devices/brachiosaurus/filesystems.nix
@@ -37,5 +37,25 @@
         "noatime"
       ];
     };
+
+    "/home/noah/XOS" = {
+      device = "/dev/disk/by-uuid/4dc1680c-da77-4515-b464-1e2c56d0a7a4";
+      fsType = "btrfs";
+      options = [
+        "rw"
+        "noatime"
+        "compress=zstd:4"
+      ];
+    };
+
+    "/home/noah/CCache" = {
+      device = "/dev/disk/by-uuid/c76cdb0a-7e73-47e6-b1fa-5e85c02435aa";
+      fsType = "btrfs";
+      options = [
+        "rw"
+        "noatime"
+        "compress=zstd:4"
+      ];
+    };
   };
 }
