0cf5324c55216c68d4f69af986091153f9389535
Noah Anleitner <noah.anleitner@nonetwor.cc>
2026-01-28 21:10:55 +0100

    Zen: add more configuration

--- modules/browser/zen.nix
+++ modules/browser/zen.nix
@@ -16,12 +16,27 @@
           isDefault = true;
           settings = {
             "zen.window-sync.enabled" = false;
+            "zen.site-data-panel.show-callout" = false;
+            "zen.theme.gradient-legacy-version" = 1;
+            "zen.ui.migration.compact-mode-button-added" = true;
+            "zen.urlbar.behavior" = "normal";
+            "zen.view.compact.enable-at-startup" = false;
+            "zen.view.show-newtab-button-border-top" = true;
+            "zen.view.show-newtab-button-top" = false;
+            "zen.view.use-single-toolbar" = false;
+            "zen.view.window.scheme" = 0;
+            "zen.workspace.separate-essentials" = false;
             "media.videocontrols.picture-in-picture.enable-when-switching-tabs.enabled" = true;
           };
           extensions.packages =
             with inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system}; [
               ublock-origin
               bitwarden
+              sponsorblock
+              startpage-private-search
+              return-youtube-dislikes
+              youtube-shorts-block
+              youtube-no-translation
             ];
         };
       };
