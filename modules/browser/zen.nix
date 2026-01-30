f91fb79e1fc8e9103bd9afee5033905e7f4a8c42
Noah Anleitner <noah.anleitner@nonetwor.cc>
2026-01-30 20:18:11 +0100

    Zen: install extensions without firefox addons flake

--- modules/browser/zen.nix
+++ modules/browser/zen.nix
@@ -1,9 +1,29 @@
 {
   inputs,
   applyHomeManagerShared,
-  pkgs,
   ...
 }:
+let
+  mkPluginUrl = id: "https://addons.mozilla.org/firefox/downloads/latest/${id}/latest.xpi";
+
+  mkExtensionEntry = {
+    id,
+    pinned ? false,
+  }: let
+    base = {
+      install_url = mkPluginUrl id;
+      installation_mode = "force_installed";
+    };
+  in
+    if pinned
+    then base // {default_area = "navbar";}
+    else base;
+
+  mkExtensionSettings = builtins.mapAttrs (_: entry:
+    if builtins.isAttrs entry
+    then entry
+    else mkExtensionEntry {id = entry;});
+in
 {
   home-manager = applyHomeManagerShared {
     imports = [
@@ -12,6 +32,34 @@
     programs = {
       zen-browser = {
         enable = true;
+        policies = {
+          ExtensionSettings = mkExtensionSettings {
+            "uBlock0@raymondhill.net" = mkExtensionEntry {
+              id = "ublock-origin";
+              pinned = true;
+            };
+            "{446900e4-71c2-419f-a6a7-df9c091e268b}" = mkExtensionEntry {
+              id = "bitwarden-password-manager";
+              pinned = true;
+            };
+            "sponsorBlocker@ajay.app" = mkExtensionEntry {
+              id = "sponsorblock";
+              pinned = false;
+            };
+            "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" = mkExtensionEntry {
+              id = "return-youtube-dislikes";
+              pinned = false;
+            };
+            "{34daeb50-c2d2-4f14-886a-7160b24d66a4}" = mkExtensionEntry {
+              id = "youtube-shorts-block";
+              pinned = false;
+            };
+            "{9a3104a2-02c2-464c-b069-82344e5ed4ec}" = mkExtensionEntry {
+              id = "youtube-no-translation";
+              pinned = false;
+            };
+          };
+        };
         profiles.default = {
           isDefault = true;
           settings = {
@@ -28,16 +76,6 @@
             "zen.workspace.separate-essentials" = false;
             "media.videocontrols.picture-in-picture.enable-when-switching-tabs.enabled" = true;
           };
-          extensions.packages =
-            with inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system}; [
-              ublock-origin
-              bitwarden
-              sponsorblock
-              startpage-private-search
-              return-youtube-dislikes
-              youtube-shorts-block
-              youtube-no-translation
-            ];
           search = {
             force = true;
             default = "Startpage";
