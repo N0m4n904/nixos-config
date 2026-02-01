e1553b46e9d64e23f3633852718dcddf14d1a0e3
Noah Anleitner <noah.anleitner@nonetwor.cc>
2026-02-01 19:38:48 +0100

    Revert "Use own fork of led-matrix"
    This reverts commit f46aff0fc3062d2b18bf965d4419b482b4feaaf3.

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     led-matrix-monitoring = {
-      url = "github:N0m4n904/led-matrix";
+      url = "github:MidnightJava/led-matrix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
