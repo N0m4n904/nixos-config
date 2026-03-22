9c5ed84ed7dc02456e006cc43f68169ac98debe9
Noah Anleitner <noah.anleitner@nonetwor.cc>
2026-03-22 19:02:32 +0100

    server: disable autologin

--- server/server.nix
+++ server/server.nix
@@ -8,9 +8,6 @@
     foundrixModules.config.virtualisation.docker
   ];
 
-  users.allowNoPasswordLogin = true;
-  services.getty.autologinUser = "noah";
-
   nix.settings.trusted-users = [ "root" "@wheel" ];
 
   boot.initrd.systemd.enable = true;
