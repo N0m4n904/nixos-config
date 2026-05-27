b8cfacfcdc74fd5765b842ef9958a8676abcd99e
Noah Anleitner <noah.anleitner@nonetwor.cc>
2026-05-27 17:11:25 +0200

    Update Proton-CachyOS

--- desktop/desktop.nix
+++ desktop/desktop.nix
@@ -36,11 +36,11 @@
         (proton-ge-bin.overrideAttrs (
           prev: final: {
             src = fetchzip {
-              url = "https://github.com/CachyOS/proton-cachyos/releases/download/cachyos-11.0-20260506-slr/proton-cachyos-11.0-20260506-slr-x86_64_v3.tar.xz";
-              hash = "sha256-eFj1eZj+aMkZyUFdcB9OdLQrrTukbaBBTlLZkwq9xNs=";
+              url = "https://github.com/CachyOS/proton-cachyos/releases/download/cachyos-11.0-20260520-slr/proton-cachyos-11.0-20260520-slr-x86_64_v3.tar.xz";
+              hash = "sha256-kUzi6ZRBNSGwipBkPvkLRPRod6V33TpmY4Jmi6IVk0Q=";
             };
             pname = "proton-cachyos";
-            version = "proton-cachyos-11.0-20260506-slr-x86_64_v3";
+            version = "proton-cachyos-11.0-20260520-slr-x86_64_v3";
           }
         )).override
         { steamDisplayName = "Proton-CachyOS-latest"; }
